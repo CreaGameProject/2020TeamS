929862aebd81de60bab36682e5e29ad50f9d3cd2
MittaniK1 <m.keichi16256@gmail.com>
2020-10-01 10:18:10 +0900

    add ButtonScript GameMaangerScript

---
+++ Assets/Scripts/GameManagerScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManagerScript : MonoBehaviour
+{
+    [System.NonSerialized] public static int playerNumber = 0;
+
+
+
+    private void Start()
+    {
+        DontDestroyOnLoad(this.gameObject);
+        SceneManager.LoadScene("Title");
+    }
+
+
+
+}
